472a72929f74f86d0039146ac2c9a364c6888223
MatthewLennie <matthewdlennie@gmail.com>
2020-04-27 10:27:25 -0400

    working with reasonable accuracy not overfitting any more learning rate schedule is awesome

--- collabfilter.py
+++ collabfilter.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from torch.utils.tensorboard import SummaryWriter
 import random
-
+import collections
 
 class collabmodel(torch.nn.Module):
     def __init__(self, movie_size, user_size, embedding_size):
@@ -40,21 +40,28 @@ class learner():
         self.ratings_test = ratings_test
         self.current_epoch = None
         self.writer = SummaryWriter('runs/{}'.format(random.randint(0, 1e9)))
-        self.optimizer = torch.optim.SGD(
-            self.model.parameters(), lr=0.05, momentum=0.9)
-
+        # self.optimizer = torch.optim.SGD(
+        #     self.model.parameters(), lr=0.05, momentum=0.9)
+        self.init_optimizer()
     def learn(self, lr=None):
         prediction = self.model.forward(
             self.users_train.to(device), self.movies_train.to(device))
         calculated_loss = self.loss(prediction, self.ratings_train.to(device))
         calculated_loss.backward()
-        if self.current_epoch % 100 == 0:
+        if self.current_epoch % 2 == 0:
             self.validate()
             self.writer.add_scalar(
                 'Training loss', calculated_loss, self.current_epoch)
+        self.schedule.step()
         self.optimizer.step()
         self.optimizer.zero_grad()
         return calculated_loss
+
+    def init_optimizer(self):
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=0.05)
+        self.schedule = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer,T_max = 200)
+        return None
 
     def validate(self):
         Validation = loss(model(self.users_test.to(device),
@@ -66,16 +73,18 @@ class learner():
             "Movie Bias Grad", self.model.bias_movie.grad)
         self.writer.add_histogram("User Bias", self.model.bias_user)
         self.writer.add_histogram("User Bias Grad", self.model.bias_user.grad)
-        self.writer.add_scalar("lr", self.optimizer.param_groups[0]['lr'])
+        self.writer.add_scalar("lr", self.optimizer.param_groups[0]['lr'],self.current_epoch)
         # print("lr: {}".format(optimizer))    return Validation
         errors = model(self.users_test.to(device),
-                                self.movies_test.to(device)) - self.ratings_test.to(device)
+                       self.movies_test.to(device)) - self.ratings_test.to(device)
         self.writer.add_histogram("validation errors", errors)
         return errors, self.movies_test
 
     def loop(self, epochs):
         for self.current_epoch in range(epochs):
             loss = self.learn()
+            if self.schedule._step_count %200 ==0:
+                self.init_optimizer()
         return loss
 
     def lr_find(self):
@@ -113,13 +122,17 @@ movies = pd.read_csv('./ml-100k/u.item', delimiter='\|', header=None)
 movies.rename(columns={0: 'item'}, inplace=True)
 movies[1].astype('str')
 
-most_common_movies = df.item.value_counts()[:100].index.tolist()
+most_common_movies = df.item.value_counts()[:20].index.tolist()
 # create a smaller df with the reduced dataset, just assume all users are present
+# relative index just relates the reduced movie index to the original dataset 'item'
 shortdf = df[df['item'].isin(most_common_movies)]
-relative_dict = {movie: i for i, movie in enumerate(most_common_movies)}
-shortdf['relative_index'] = [relative_dict[movie]
-                             for movie in shortdf['item'].values]
-#
+# .drop('index').reset_index()
+shortmovie = movies[movies['item'].isin(most_common_movies)].reset_index()
+shortmovie.drop('index', axis=1, inplace=True)
+shortmovie.reset_index(inplace=True)
+shortmovie.rename(columns={'index': 'relative_index'}, inplace=True)
+shortdf = shortdf.merge(shortmovie, on='item', how='inner', validate='m:1')
+
 X_train, X_test, y_train, y_test = train_test_split(
     shortdf[['user', 'relative_index']].values, shortdf['rating'].astype('float').values)
 # TODO: use pytorch dataloaders, this is messy
@@ -133,28 +146,38 @@ ratings_test = torch.tensor(y_test).float()
 
 loss = torch.nn.MSELoss()
 
-model = collabmodel(len(most_common_movies), len(df['user'].unique())+1, 30)
+model = collabmodel(len(most_common_movies), len(df['user'].unique())+1, 4)
 
 model.to(device)
 collab_learn = learner(model, loss, users_train, users_test,
                        movies_train, movies_test, ratings_train, ratings_test)
 
-collab_learn.loop(4000)
+collab_learn.loop(50)
+# show best and worst movie biases
+shortmovie['bias'] = collab_learn.model.bias_movie.squeeze(
+).cpu().detach().numpy()
+collab_learn.writer.add_text('best_movies', shortmovie.sort_values(
+    'bias').tail(10)[['bias', 1]].to_markdown())
+collab_learn.writer.add_text('worst_movies', shortmovie.sort_values(
+    'bias').head(10)[['bias', 1]].to_markdown())
 
-movies = movies.set_index('item')
-for value, key in enumerate(relative_dict):
-    movie_name = movies.loc[key, 1]
-
-    print("movie rank: {} has score {}".format(movie_name,
-                                               collab_learn.model.bias_movie.squeeze()[value]))
-
+# start inspecting the errors
 losses, labels = collab_learn.validate()
-flipped_relative = dict(zip(relative_dict.values(), relative_dict.keys()))
-current_movies = [movies.loc[flipped_relative[l.detach().item()],1] for l in labels]
-losses_df = pd.DataFrame([losses.to('cpu').detach().numpy(),current_movies]).T
+
+losses_df = pd.DataFrame(
+    [losses.to('cpu').detach().numpy(), shortmovie.loc[labels][1]]).T
 losses_df['abs_loss'] = abs(losses_df[0])
-for i in range(20):
-    collab_learn.writer.add_text('worst underpredictions','{}'.format(losses_df.sort_values(0).iloc[i]))
-    collab_learn.writer.add_text('worst overpredictions','{}'.format(losses_df.sort_values(0).iloc[-i]))
-    collab_learn.writer.add_text('best predictions','{}'.format(losses_df.sort_values('abs_loss').iloc[i]))
-a = 1
+losses_df = losses_df.infer_objects()
+print(losses_df.describe())
+collab_learn.writer.add_text('movie losses descriptions', losses_df.groupby(
+    1).describe()[0].sort_values('std').to_markdown())
+
+
+# for i in range(20):
+#     collab_learn.writer.add_text(
+#         'worst underpredictions', '{}'.format(losses_df.sort_values(0).iloc[i]))
+#     collab_learn.writer.add_text(
+#         'worst overpredictions', '{}'.format(losses_df.sort_values(0).iloc[-i]))
+#     collab_learn.writer.add_text('best predictions', '{}'.format(
+#         losses_df.sort_values('abs_loss').iloc[i]))
+# a = 1
